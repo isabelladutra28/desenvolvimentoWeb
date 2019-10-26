ea4dff4a29e7fe0f65fe9c091fd7e1fbbf77b83a
isabelladutra28 <48951775+isabelladutra28@users.noreply.github.com>
2019-10-26 17:27:11 -0300

    Create Produto.php

---
+++ Produto.php
@@ -0,0 +1,42 @@
+<?php
+include "InformacaoNulaException.php";
+abstract class Produto{
+	private $codigo;
+	private $preco;
+
+	public function Produto($codigo, $preco){
+	$this->_constructor($codigo, $preco);
+   }
+
+}
+
+public function _constructor($codigo, $preco){
+	if(is_null($codigo) || is_null($preco)){
+			throw new InformacaoNulaException();
+		}else{
+$this->codigo = $codigo;
+		$this->preco = $preco;	
+	}
+	
+}
+
+
+	public function __get($atributo){
+		return $this->$atributo;
+	}
+	public function getCodigo(){
+		return $this->codigo;
+	}
+	public function getPreco(){
+		return $this->preco;
+	}
+
+	public function setCodigo($codigo){
+		$this->codigo = $codigo;
+	}
+	
+	public function setPreco($preco){
+		$this->preco = $preco;
+	}	
+}
+?>
